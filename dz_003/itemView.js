bdf92eb6f33b763987ffdacd152ba24d0e414888
allealena <allealena@yandex.ru>
2017-02-14 12:40:44 +0200

    Delete itemView.js

--- dz_003/itemView.js
+++
@@ -1,9 +0,0 @@
-function ItemView (studentProperties, student) {
-    this.row = '';
-    studentProperties.forEach(function(item) {
-        row += tmplItem(student[item]);
-    });
-
-    return this;
-}
-
